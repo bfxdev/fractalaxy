ed7cbf3d8070c128b992c07e7378ab572d78679d
bfxdev <11587816+bfxdev@users.noreply.github.com>
2021-11-03 09:21:55 +0100

    Added log text area to track Android multi-touch

--- FractalRenderer.gd
+++ FractalRenderer.gd
@@ -6,8 +6,11 @@ export var Rotation : Vector3 = Vector3(0.0, 0.0, 0.0)
 export var ZoomFactor : float = 1.1
 
 var dragging : bool = false
-var dragpos : Vector2
+var dragging_position : Vector2
 
+# We need maximum 3 fingers
+var touch_pressed = [false, false, false]
+var touch_position = [] 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,12 +25,15 @@ func _process(_delta):
 	material.set_shader_param("Radius", Radius)
 	material.set_shader_param("Rotation", Rotation)
 
-func _on_FractalRenderer_gui_input(event: InputEvent) -> void:
+func label_print(string):
+	$Label.text = string + "\n" + $Label.text
+
+
+func _gui_input(event: InputEvent) -> void:
 	
 	var dimension = 2.0*Radius/min(rect_size.x, rect_size.y)
 
 	if event is InputEventMouseButton:
-		print("Center:", Center, "  Radius:", Radius)
 		if event.pressed and event.button_index == BUTTON_WHEEL_UP:
 			Radius /= ZoomFactor
 			var delta = event.position-0.5*rect_size
@@ -39,22 +45,123 @@ func _on_FractalRenderer_gui_input(event: InputEvent) -> void:
 			Center -= (ZoomFactor-1)*delta*dimension
 			Radius *= ZoomFactor
 		if event.pressed  and event.button_index in [BUTTON_LEFT,BUTTON_MIDDLE]:
-			dragpos = event.position
+			dragging_position = event.position
 			dragging = true
 		if not event.pressed  and event.button_index in [BUTTON_LEFT,BUTTON_MIDDLE]:
 			dragging = false
 	
 	if event is InputEventMouseMotion and dragging:
-		var delta = event.position-dragpos
+		var delta = event.position-dragging_position
 		delta.y *= -1.0
 		Center -= delta*dimension
+		dragging_position = event.position
+
+	if event is InputEventScreenTouch:
+		label_print("TOUCH: " + str(event.index) + " " + str(event.pressed) + " at " +
+					 str(event.position))
+		touch_pressed[event.index] = event.pressed
+		touch_position[event.index] = event.position
 		
-		dragpos = event.position
-		
+		if touch_pressed[1]:
+			pass
+
+	if event is InputEventScreenDrag:
+		label_print("DRAG: " + str(event.index) + " at " + str(event.position) + " relative " + 
+					 str(event.relative))
+
+	#$Label.text = "Center " + str(Center) + "\nRadius " + str(Radius) + "\n" + $Label.text 
+	
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Copied from Multitouch Cubes Demo
+func dummy():
+	var base_state
+	var event
+	var curr_state
+	
+	var finger_count = base_state.size()
+
+	if finger_count == 0:
+		# No fingers => Accept press.
+		if event is InputEventScreenTouch:
+			if event.pressed:
+				# A finger started touching.
+
+				base_state = {
+					event.index: event.position,
+				}
+
+	elif finger_count == 1:
+		# One finger => For rotating around X and Y.
+		# Accept one more press, unpress or drag.
+		if event is InputEventScreenTouch:
+			if event.pressed:
+				# One more finger started touching.
+
+				# Reset the base state to the only current and the new fingers.
+				base_state = {
+					curr_state.keys()[0]: curr_state.values()[0],
+					event.index: event.position,
+				}
+			else:
+				if base_state.has(event.index):
+					# Only touching finger released.
+
+					base_state.clear()
+
+		elif event is InputEventScreenDrag:
+			if curr_state.has(event.index):
+				# Touching finger dragged.
+				# Since rotating around two axes, we have to reset the base constantly.
+				curr_state[event.index] = event.position
+				base_state[event.index] = event.position
+
+	elif finger_count == 2:
+		# Two fingers => To pinch-zoom and rotate around Z.
+		# Accept unpress or drag.
+		if event is InputEventScreenTouch:
+			if not event.pressed and base_state.has(event.index):
+				# Some known touching finger released.
+
+				# Clear the base state
+				base_state.clear()
+
+		elif event is InputEventScreenDrag:
+			if curr_state.has(event.index):
+				# Some known touching finger dragged.
+				curr_state[event.index] = event.position
+
+				# Compute base and current inter-finger vectors.
+				var base_segment = base_state[base_state.keys()[0]] - base_state[base_state.keys()[1]]
+				var new_segment = curr_state[curr_state.keys()[0]] - curr_state[curr_state.keys()[1]]
+
+				# Get the base scale from the base matrix.
+
+
+	# Finger count changed?
+	if base_state.size() != finger_count:
+		# Copy new base state to the current state.
+		curr_state = {}
+		for idx in base_state.keys():
+			curr_state[idx] = base_state[idx]
+		# Remember the base transform.
 
 
